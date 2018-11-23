41a014d46beca74551e5cf9d5c075b855a34c3cb
aprizalabyan <43059793+aprizalabyan@users.noreply.github.com>
2018-11-23 16:07:22 +0800

    update 23 Nov (2)

--- Assets/Scripts/tabrakan.cs
+++ Assets/Scripts/tabrakan.cs
@@ -3,19 +3,37 @@ using UnityEngine;
 
 public class tabrakan : MonoBehaviour {
 
-	public GameObject playerExplosion;
 	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private gameKontrol gameController;
 
-	void OnTriggerEnter(Collider other){
-		if (other.tag == "Boundary" || other.tag == "Enemy") {
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <gameKontrol>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
 			return;
 		}
-		if (other.tag == "Player") {
-			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
 		}
-
-		Instantiate (explosion, transform.position, transform.rotation);
-		Destroy (other.gameObject);
-		Destroy (gameObject);
+		//gameController.AddScore (scoreValue);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
 	}
 }
